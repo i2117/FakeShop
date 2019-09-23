c56d0a38d5da824e0b93224b414e1d8e733aaedc
Ilya <Ilya@@@>
2019-09-23 09:19:00 +0300

    My first commit

---
+++ app.js
@@ -0,0 +1,73 @@
+const request = require("request")
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const products = JSON.parse(fs.readFileSync('products.json', 'utf8')).products;
+const currencyUrl = "https://www.cbr-xml-daily.ru/daily_json.js"
+
+let currencyData;
+
+let getCurrencyData = () => {
+  request({
+    url: currencyUrl,
+    json: true
+  }, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      currencyData = body.Valute
+      console.log("Updated currencyData")
+    }
+  })
+}
+
+getCurrencyData()
+
+const CronJob = require('cron').CronJob;
+const job = new CronJob('0 */30 * * * *', function() {
+    getCurrencyData()
+  }
+)
+job.start()
+
+let toCurrency = (value, currency) => {
+  const curr = currencyData[currency]
+  let coeff = 1
+  if (curr)
+    coeff = curr.Nominal / curr.Value
+
+  return (value * coeff).toFixed(2)
+}
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public/'));
+
+app.get('/products', function (req, res) {
+  res.send(products);
+}); 
+
+app.get('/currencyData', function (req, res) {
+  res.send(currencyData);
+}); 
+
+app.post('/countProductsPrice', function (req, res) {
+  const products = req.body
+  console.log(products)
+
+  const priceRUB = products.reduce((a, b) => a + b.price * b.quantity, 0)
+  let result = {
+    RUB: toCurrency(priceRUB, "RUB"),
+    EUR: toCurrency(priceRUB, "EUR"),
+    USD: toCurrency(priceRUB, "USD")
+  }
+  res.send(result);
+}); 
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
---
+++ public/index.jsx
@@ -0,0 +1,167 @@
+class Product extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quantity: 0,
+      price: toCurrency(props.priceRUB, "RUB"),
+      currency: "RUB"
+    }
+  }
+
+  changeQuantity = (amount) => {
+    const newQuantity = this.state.quantity + amount
+    this.setState((state) => {
+      return {quantity: newQuantity};
+    });
+    this.props.onQuantityChange(this.props.id, newQuantity);
+  }
+
+  changeCurrency = (currencyObj) => {
+    const currency = currencyObj.target.value
+    const newPrice = toCurrency(this.props.priceRUB, currency)
+
+
+    this.setState((state) => {
+      return {price: newPrice};
+    });
+  }
+
+  render() {
+    return <div class="card" style={{width: "350px"}}>
+      <h1 class="card-header">{this.props.name}</h1>
+
+      <div class="card-body col">
+        <div class="row m-1">
+          <select class="btn btn-primary dropdown-toggle m-2" onChange={this.changeCurrency}>
+            <option value="RUB">RUB</option>
+            <option value="EUR">EUR</option>
+            <option value="USD">USD</option>
+          </select>
+          <h2 class="col px-md-2">{this.state.price}</h2>
+        </div>
+
+        <div class="row m-1">
+          <button class="btn btn-primary col-sm m-2" onClick={() => this.changeQuantity(-1)} disabled={this.state.quantity <= 0}>-</button>
+          <div class="badge badge-primary text-wrap col-sm m-2">
+            <h2>{this.state.quantity}</h2>
+          </div>
+          <button class="btn btn-primary col-sm m-2" onClick={() => this.changeQuantity(1)}>+</button>
+        </div>
+      </div>
+
+    </div>
+  }
+}
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props)
+    let products = this.props.products.map((el) => {
+      return {...el, quantity: 0}
+    })
+    this.state = {
+      products: products
+    }
+  }
+
+  handleQuantityChange = (productID, value) => {
+    const newState = this.state
+    newState.products[productID].quantity = value
+    this.setState(newState)
+    console.log(newState)
+  }
+
+  countProductsPrice = () => {
+    let xhr = new XMLHttpRequest();
+    xhr.open('POST', ADDRESS + "countProductsPrice");
+    xhr.setRequestHeader("Content-type", "application/json");
+    xhr.send(JSON.stringify(this.state.products));
+    xhr.onload = function() {
+      if (xhr.status != 200) {
+        alert(`Error ${xhr.status}: ${xhr.statusText}`)
+      } else {
+        const res = JSON.parse(xhr.response)
+        alert(`
+          Total price: \n 
+          RUB: ${res.RUB} \n 
+          EUR: ${res.EUR} \n 
+          USD: ${res.USD} 
+        `)
+      }
+    }
+  }
+
+  render() {
+    const productList = this.props.products.map((el, i) => {
+      return <
+        Product 
+          id={i}
+          name={el.name} 
+          priceRUB={el.price} 
+          onQuantityChange = {this.handleQuantityChange}
+          class="col">
+        </Product>
+    })
+    return (
+      <div>
+        <div class="row">
+          <button class="btn btn-primary btn-lg m-5" onClick={this.countProductsPrice}>
+            Посчитать
+          </button>
+        </div>
+
+        <div  class="container row">
+          {productList}
+        </div>
+
+      </div>
+    )
+  }
+}
+
+var toCurrency = (value, currency) => {
+  const curr = currencyData[currency]
+  let coeff = 1
+  if (curr)
+    coeff = curr.Nominal / curr.Value
+
+  return (value * coeff).toFixed(2)
+}
+
+const ADDRESS = window.location.href //"http://" + location.host
+console.log(ADDRESS)
+
+// Getting products data
+const http = new XMLHttpRequest()
+
+let products = []
+
+http.open("GET", ADDRESS + "products")
+http.send()
+
+http.onload = () => {
+  products = JSON.parse(http.response)
+  console.log(products)
+  ReactDOM.render(
+    <div>
+      <ProductList products={products}></ProductList>
+    </div>,
+    document.getElementById("app")
+  )
+}
+
+// Getting currency data
+const currencyRequest = new XMLHttpRequest()
+
+var currencyData = {}
+
+currencyRequest.open("GET", ADDRESS + "currencyData")
+currencyRequest.send()
+
+currencyRequest.onload = () => {
+  currencyData = JSON.parse(currencyRequest.response)
+  console.log(currencyData)
+}
+
+
+
